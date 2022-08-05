1378dee6b1e72225c12b69e5b3cc3a7dad51a096
rebeca0017 <frc.riofrio@yavirac.edu.ec>
2022-08-05 13:25:34 -0500

    arreglo seguridad app

--- .history/app_20220804192831.js
+++ .history/app_20220804192831.js
@@ -1,12 +1,11 @@
 const express = require('express')
-const app1 = express()
-app1.disable("x-powered-by");
+const app = express()
 const port = 3000
 
-app1.get('/', (req, res) => {
+app.get('/', (req, res) => {
   res.send('hola mundo ')
 })
 
-app1.listen(port, () => {
+app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
